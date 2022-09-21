3ccd6dda0ce05d8c71b6445e2db0738952f43b9a
mona@lisa.com <ricca1.rp@gmail.com>
2022-09-21 17:14:48 +0200

    inizio creazione sito

--- cvsite/models.py
+++ cvsite/models.py
@@ -4,9 +4,9 @@ import datetime
 
 # Create your models here.
 class cvelement(models.Model):
-    nome=models.CharField(max_length=60, defoult=None)
-    riassunto=models.CharField(max_length=60, defoult=None)
-    descrizione=models.CharField(max_length=500, defoult=None) 
+    nome=models.CharField(max_length=60, default=None)
+    riassunto=models.CharField(max_length=60, default=None)
+    descrizione=models.CharField(max_length=500, default=None) 
     start = models.DateField(("Date"), default=datetime.date.today)
     end = models.DateField(("Date"), default=datetime.date.today)
 
